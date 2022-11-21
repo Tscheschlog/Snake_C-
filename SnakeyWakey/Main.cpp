afc6b85ce6f03bac47be48205801b1695e0c9f42
Tscheschlog <Chris Tscheschlog>
2022-11-21 00:13:18 -0500

    Menu Music Test

--- SnakeyWakey/Main.cpp
+++ SnakeyWakey/Main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <SFML/Audio/Music.hpp>
 #include "Snake.h"
 #include "Apple.h"
 #include "Menu.h"
 #include "GameBoard.h"
 
 
+
+
+
 int main() {
+
+    // Declare a new music
+    sf::Music music;
+    // Open it from an audio file
+    if (!music.openFromFile("Music/music.ogg"))
+    {
+        // error...
+        std::cout << "MUSIC FAILED!\n";
+    }
+    // Change some parameters
+    music.setPosition(0, 1, 10); // change its 3D position
+    music.setPitch(2);           // increase the pitch
+    music.setVolume(50);         // reduce the volume
+    music.setLoop(true);         // make it loop
+    // Play it
+    music.play();
+
 
     std::srand(time(0));
 
